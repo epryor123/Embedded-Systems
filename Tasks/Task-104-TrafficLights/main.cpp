f68e6fdb6323227fbcae55b34a768487665b5b88
epryor123 <146339633+epryor123@users.noreply.github.com>
2023-10-05 16:00:56 +0100

    103&104

--- Tasks/Task-104-TrafficLights/main.cpp
+++ Tasks/Task-104-TrafficLights/main.cpp
@@ -11,27 +11,40 @@ LCD_16X2_DISPLAY lcd;
 
 int main()
 {
-    lcd.puts("RED");
-    wait_us(1000000);
+    red = 0;
+    amber = 0;
+    green = 0;
 
-    amber = 1;
-    lcd.cls();
-    lcd.puts("Amber");
-    wait_us(1000000);
-
-    green = 1;
-    lcd.cls();
-    lcd.puts("Green");    
-    wait_us(1000000);
-
-    lcd.cls();
-    lcd.puts("TASK-104");
+    //lcd.cls();
+    //lcd.puts("TASK-104");
 
     while (true)
     {
-        red = !red;
-        amber = !amber;
-        green = !green;
-        wait_us(WAIT_TIME_MS * 1000);
+        red = 1;
+        lcd.cls();
+        lcd.puts("RED");
+        wait_us(10000000);
+
+        amber = 1;
+        lcd.cls();
+        lcd.puts("Amber");
+        wait_us(2000000);
+        red = 0;
+        amber = 0;
+
+        green = 1;
+        lcd.cls();
+        lcd.puts("Green");    
+        wait_us(10000000);
+        green = 0;
+
+        amber = 1;
+        lcd.cls();
+        lcd.puts("Amber");
+        wait_us(2000000);
+        amber = 0;
+
+
+
     }
 }
