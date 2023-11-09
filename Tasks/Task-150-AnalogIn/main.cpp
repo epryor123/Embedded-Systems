3fb22b6dafd0ac1e5d93da2fe86634dd1540abb7
epryor123 <146339633+epryor123@users.noreply.github.com>
2023-11-09 15:54:48 +0000

    Task 150

--- Tasks/Task-150-AnalogIn/main.cpp
+++ Tasks/Task-150-AnalogIn/main.cpp
@@ -14,32 +14,15 @@ AnalogIn pot(AN_POT_PIN);
 AnalogIn ldr(AN_LDR_PIN);
 AnalogIn mic(MIC_AN_PIN);
 
+DigitalOut redLED(TRAF_RED1_PIN);
+DigitalOut yellowLED(TRAF_YEL1_PIN);
+DigitalOut greenLED(TRAF_GRN1_PIN);
+
 int main()
 {
 
     //Test LED Bar Display
     ledDisp.enable(true);
-
-    ledDisp.setGroup(LatchedLED::LEDGROUP::RED);
-    for (unsigned int n=0; n<8; n++) {
-        ledDisp = 1 << n;
-        wait_us(250000);
-    }
-    ledDisp = 0;
-
-    ledDisp.setGroup(LatchedLED::LEDGROUP::GREEN);
-    for (unsigned int n=0; n<8; n++) {
-        ledDisp = 1 << n;
-        wait_us(250000);
-    }
-    ledDisp = 0;
-    
-    ledDisp.setGroup(LatchedLED::LEDGROUP::BLUE);
-    for (unsigned int n=0; n<8; n++) {
-        ledDisp = 1 << n;
-        wait_us(250000);
-    }     
-    ledDisp = 0;
 
 
     while (true) {
@@ -48,6 +31,35 @@ int main()
         unsigned short lightVal = ldr.read_u16();
         unsigned short micVal   = mic.read_u16(); 
 
+        
+
+        if (potVal > 0x8000){
+            ledDisp.setGroup(LatchedLED::LEDGROUP::RED);
+            for (unsigned int n=0; n<8; n++) {
+                ledDisp = 1 << n;
+                wait_us(250000);
+            }
+            ledDisp = 0;
+        }
+
+        if (lightVal < 0x1000){
+            ledDisp.setGroup(LatchedLED::LEDGROUP::GREEN);
+            for (unsigned int n=0; n<8; n++) {
+                ledDisp = 1 << n;
+                wait_us(250000);
+            }
+            ledDisp = 0;
+        }        
+
+        if (micVal > 0x8500){
+            ledDisp.setGroup(LatchedLED::LEDGROUP::BLUE);
+            for (unsigned int n=0; n<8; n++) {
+                ledDisp = 1 << n;
+                wait_us(250000);
+            }     
+            ledDisp = 0;
+        }
+
         //Write to terminal
         printf("--------------------------------\n");
         printf("Potentiometer: %X\n", potVal);
@@ -55,7 +67,7 @@ int main()
         printf("Microphone: %X\n", micVal);   
 
         //Wait 0.25 seconds
-        wait_us(500000);
+        wait_us(250000);
 
     }
 }
